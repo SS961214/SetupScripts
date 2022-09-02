b5f9f2f9a31bde8a6540d35e70d1b17b7b767d23
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-09-02 20:21:13 +0900

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# <<< Install VScode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc \
+	| gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+apt install apt-transport-https
+apt update && apt install code
+# >>> Install VScode
+
+
 # <<< Install docker
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
@@ -9,3 +20,18 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 # >>> Install docker
+
+
+# <<< Install nvidia container toolkit
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+if lspci | grep -i nvidia; then
+	distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+		&& curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+		&& curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+		sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+		sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+	apt update;
+	apt install -y nvidia-docker2;
+	systemctl restart docker;
+fi
+# >>> Install nvidia container toolkit
