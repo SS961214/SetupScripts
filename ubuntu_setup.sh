2420d80dd8f3e6601c64373e657a6f4a7550fd4d
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-09-02 19:05:27 +0900

    Add files via upload

---
+++ ubuntu_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# <<< Install docker
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+	| gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+	| tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# >>> Install docker
