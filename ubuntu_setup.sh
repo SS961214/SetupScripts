2ec3e44ddde7e578671e25a0d4fefcd136aaf335
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-09-03 01:10:03 +0900

    Update ubuntu_setup.sh needs tests

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19,6 +19,8 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 	| tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#echo groupadd docker
+#echo usermod -aG docker $USER
 # >>> Install docker
 
 
